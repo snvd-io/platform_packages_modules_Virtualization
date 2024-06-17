fef607cf0fc6602c1dc18f16739e5607b3ddb032
Luca Stefani <luca.stefani.ge1@gmail.com>
2024-06-17 17:54:26 +0200

    Ferrochrome: Don't set kernel
    With u-boot as the default bootloader we don't
    need the standalone kernel anymore.
    
    Test: Start ferrochrome launcher
    Change-Id: I3365491ec933be7749292408c776927453f808c1

--- ferrochrome_app/custom_vm_setup.sh
+++ ferrochrome_app/custom_vm_setup.sh
@@ -2,11 +2,9 @@
 
 function copy_files() {
   cp -u /sdcard/vm_config.json /data/local/tmp
-  cp -u /sdcard/vmlinuz /data/local/tmp
   cp -u /sdcard/chromiumos_test_image.bin /data/local/tmp
   chmod 666 /data/local/tmp/vm_config.json
   chmod 666 /data/local/tmp/chromiumos_test_image.bin
-  chmod 666 /data/local/tmp/vmlinuz
 }
 setprop debug.custom_vm_setup.done false
 copy_files
